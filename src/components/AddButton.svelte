7898638f55e93f73967ff4151c2f1b9c603b1214
Korney Gedert <kiper@altlinux.org>
2025-05-17 17:58:48 +0400

    wip: improve add button.
    - feat: "Enter" key event for add
    - feat: class forwarding
    - feat: onadd event.

--- src/components/AddButton.svelte
+++ src/components/AddButton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import {slide} from "svelte/transition";
     import {Button, buttonVariants} from "$lib/components/ui/button";
     import {Label} from "$lib/components/ui/label";
     import {Input} from "$lib/components/ui/input";
@@ -7,11 +8,51 @@
     import * as Dialog from "$lib/components/ui/dialog";
     import HolderPromptInput from "./HolderPromptInput.svelte";
 
-    let open = $state(false);
+    interface Props {
+        open: boolean;
+        variant: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | undefined;
+        onadd?: (name: string) => void;
+        ignore: string[];
+        class: string;
+    }
+
+    let {
+        open = $bindable(false),
+        variant = "link",
+        class: className = "",
+        onadd = undefined,
+        ignore = [],
+        ...restProps
+    }: Props = $props();
+
+    let name = $state("");
+    let disabled = $derived(buttonIgnore(name));
+    let exist = $derived(!(!name || name === "" || name.trim().length === 0));
+
+    function buttonIgnore(name: string): boolean {
+        return ignore.findIndex(a => a == name.trim()) !== -1;
+    }
+
+    function enterEvent(event: KeyboardEvent) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            if (!disabled && exist) {
+                onclick();
+            }
+        }
+    }
+
+    function onclick() {
+        if (onadd) {
+            onadd(name.trim());
+        }
+        open = false;
+    }
+
 </script>
 
 <Dialog.Root bind:open={open}>
-    <Dialog.Trigger class={buttonVariants({variant:"secondary", color:"green", size:"icon"})}>
+    <Dialog.Trigger {...restProps} class={cn(buttonVariants({variant: variant, size:"icon"}), className)}>
         <Plus/>
     </Dialog.Trigger>
     <Dialog.Content>
@@ -19,9 +60,20 @@
             <Dialog.Title>Add Profile</Dialog.Title>
         </Dialog.Header>
         <Label for="name">Profile Name</Label>
-        <HolderPromptInput type="text" id="name" prompts={["default", "ADMX", "DC", "Group Policies"]}/>
+        <HolderPromptInput bind:value={name} id="name" onkeydown={enterEvent}
+                           prompts={["default", "ADMX", "DC", "Group Policies"]}
+                           type="text"/>
+        <div>
+            {#if disabled}
+                <p class="text-red-500 text-xs" transition:slide>this name is taken</p>
+            {/if}
+        </div>
         <Dialog.Footer>
-            <Button onclick={() => open = false}>Add</Button>
+            {#if exist}
+                <div transition:slide>
+                    <Button onclick={onclick} disabled={disabled}>Add</Button>
+                </div>
+            {/if}
         </Dialog.Footer>
     </Dialog.Content>
 </Dialog.Root>
