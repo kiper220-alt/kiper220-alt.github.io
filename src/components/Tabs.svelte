facc354222185807b2d84587375c2ff55cfc0872
kiper220-alt <gedertkj@basealt.ru>
2025-06-10 12:09:14 +0400

    wip: improve `Tabs.svelte` and fix bug with event propagation

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -1,46 +1,35 @@
 <script lang="ts">
     import X from "@lucide/svelte/icons/x";
     import AddButton from "$components/AddButton.svelte";
+    import { cn } from "$lib/utils";
 
     interface Props {
         tabs: string[],
         activeTab: number,
-        onadd?: (tab: string) => void,
+        onadd?: (tab: string, packages: string[]) => void,
         onremove?: (index: number, tab: string) => void,
+        class: string
     }
 
     let {
-        tabs = $bindable([]),
+        tabs = [],
         activeTab = $bindable(0),
         onadd: onAdd = undefined,
-        onremove: onRemove = undefined
+        onremove: onRemove = undefined,
+        class: className = '',
     }: Props = $props();
 
-    let tabsOrder = $state([...tabs]);
-
-    function closeTab(event: MouseEvent, index: number) {
-        event.preventDefault();
-        event.stopPropagation();
-        if (tabs.length === 0) return;
-
-        if (tabs.length === 1) {
-            tabs = [];
-            return;
-        }
-        tabs = [...tabs.slice(0, index), ...tabs.slice(index + 1)];
-        if (activeTab >= index) {
-            activeTab = index > 0 ? index - 1 : index;
-        }
-    }
-
-    function onadd(name: string) {
+    function onadd(name: string, packages: string[]) {
         tabs = [...tabs, name];
+        activeTab = tabs.length - 1;
         if (onAdd) {
-            onAdd(name);
+            onAdd(name, packages);
         }
     }
 
     function onremove(e: MouseEvent, index: number) {
+        e.stopPropagation();
+        e.preventDefault();
         if (onRemove) {
             onRemove(index, tabs[index]);
         }
@@ -48,11 +37,12 @@
 
 </script>
 
-<div class="flex flex-row max-h-max items-center overflow-y-auto w-full border-b overflow-x-auto">
+<div class={cn("flex flex-row max-h-max items-center overflow-y-auto w-full border-b overflow-x-auto", className)}>
     {#each tabs as tab, i (tab)}
-        <button
+        <!-- svelte-ignore a11y_click_events_have_key_events -->
+        <span
                 class="flex flex-row items-center min-w-min overflow-hidden overflow-x-auto px-2 py-2 border-r cursor-pointer transition-colors {activeTab === i ? 'bg-accent' : 'hover:bg-secondary'}"
-                type="button"
+                role="button"
                 onclick={(e) => {e.stopPropagation(); e.preventDefault(); activeTab = i;}}
                 tabindex="0"
                 aria-pressed={activeTab === i}
@@ -69,7 +59,7 @@
             >
                 <X size="12"/>
             </span>
-        </button>
+        </span>
     {/each}
     <AddButton
             class="sticky right-0 m-2 min-h-7 min-w-7 h-7 w-7"
