08a973f5d93f375f810c9d5b4963073f54ee603e
kiper220-alt <gedertkj@basealt.ru>
2025-07-04 11:53:29 +0400

    wip: move settings and search field to tabs navbar

--- src/components/TabbedPackageTable.svelte
+++ src/components/TabbedPackageTable.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
     import PackageTable from "./PackageTable.svelte";
-    import type { PackageGroup } from "./settings";
+    import { defaultBranchSettings, listFromBranchSettings, type BranchSettings, type PackageGroup } from "./settings";
     import Tabs from "./Tabs.svelte";
+    import SettingsDialog from "./SettingsDialog.svelte";
+    import SearchField from "./SearchField.svelte";
 
     interface Props {
         packages: PackageGroup[];
-        branches: string[];
+        branches: BranchSettings;
         tab: number;
         dndSort: boolean;
     }
 
     let {
         packages = $bindable([]),
-        branches = [],
+        branches = $bindable({branches: defaultBranchSettings}),
         tab = $bindable(0),
-        dndSort = true,
+        dndSort = $bindable(true),
     }: Props = $props();
 
+    let branchList = $derived(listFromBranchSettings(branches));
     let groupNames = $derived(packages.map(p => p.name));
 
     function onadd(name: string, pkgs: string[]) {
@@ -36,8 +39,13 @@
         packages[new_position] = temp;
     }
 </script>
-
-<Tabs tabs={groupNames} bind:activeTab={tab} {onadd} {onrerange} {onremove} class="flex-shrink-0"/>
+<div class="flex items-center shrink-0 justify-stretch border-b gap-2 w-screen overflow-hidden flex-row">
+    <SettingsDialog bind:branches={branches} bind:dndSort={dndSort}/>
+    <Tabs tabs={groupNames} bind:activeTab={tab} {onadd} {onrerange} {onremove} class="flex-shrink-0"/>
+    {#if groupNames.length !== 0}
+        <SearchField branches={branchList} bind:packages={packages}/>
+    {/if}
+</div>
 {#if packages.length !== 0}
-<PackageTable branches={branches} {dndSort} bind:packages={packages[tab].packages}/>
+<PackageTable branches={branchList} {dndSort} bind:packages={packages[tab].packages}/>
 {/if}
